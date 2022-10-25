5fc795ddd8bff8d38fa2d1502bca93443850b0d3
martvelasquez <113405472+martvelasquez@users.noreply.github.com>
2022-10-25 18:43:58 -0500

    arriba

--- Trabajofindesemana/scriptmltp.js
+++ Trabajofindesemana/scriptmltp.js
@@ -1,36 +1,13 @@
-      /* function test(arg1, arg2 = 'X'){
-          
-          for (let i=0; i < arg1; i++){
-              let square = ""
-              for(let j=0; j < arg1; j++){
-                  
-                  square = square  + arg2 
-                              }
-          console.log(square)
-          }
-                    
-         }*/
-
-      function test(arg1, arg2) {
-          if (!arg2) {
-              for (let i = 0; i < arg1; i++) {
-                  let square = ""
-                  for (let j = 0; j < arg1; j++) {
-
-                      square = square + 'X'
-                  }
-                  console.log(square)
-              }
-          } else {
-
-              for (let i = 0; i < arg1; i++) {
-                  let square = ""
-                  for (let j = 0; j < arg1; j++) {
-
-                      square = square + arg2
-
-                  }
-                  console.log(square)
-              }
-          }
-      }
+function test(arg1, arg2 = 'X') {
+    if (typeof arg1 === 'number') {
+        for (let i = 0; i < arg1; i++) {
+            let square = ""
+            for (let j = 0; j < arg1; j++) {
+                square = square + arg2
+            }
+            console.log(square)
+        }
+    } else if (typeof arg1 === 'undefined') {
+        console.log("Missing Size")
+    }
+}
